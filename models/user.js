df6206fc00c9325a49c9a4d3449f2b2e433079fe
Dustin <dustin.anthony.coffman@gmail.com>
2019-11-26 14:34:56 -0600

    first commit

---
+++ models/user.js
@@ -0,0 +1,80 @@
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const powerstats = new Schema({
+    intelligence: String,
+    strength: String,
+    speed: String,
+    durability: String,
+    power: String,
+    combat: String
+})
+
+const biography = new Schema ({
+    full-name: String,
+    aliases: [String],
+    place-of-birth: String,
+    first-appearance: String,
+    publisher: String,
+    alignment: String
+})
+
+const appearance = new Schema ({
+    gender: String,
+    race: String,
+    height: [String],
+    weight: [String],
+    eye-color: String,
+    hair-color: String
+})
+
+const work = new Schema ({
+    occupation: String,
+    base: String
+  })
+
+const connections = new Shema ({
+  group-affiliation: String,
+  relatives: String
+})
+
+const superHeroSchema = new Schema({
+    name: {
+        type: String
+    },
+    powerstats: [powerstats],
+
+    biography: [biography],
+
+    appearance: [appearance],
+
+    work: [work],
+
+    connections: [connections],
+
+    image: String
+
+  });
+
+
+const userSchema = new Schema({
+    name: {
+        type: String
+    },
+    
+    email: String,
+
+    collection: {
+       
+        superHero: [superHeroSchema] 
+
+    },
+    googleId: String
+}, {
+    timestamps: true
+  });
+
+
+
+
+module.exports = mongoose.model('User', userSchema); 
---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');  //this might be moved to the controller
+var passport = require('passport');
+
+const rootURL = 'https://superheroapi.com/';
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {userData: null });
+});
+
+ // Google OAuth login route
+ router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+ // Google OAuth callback route
+ router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/',
+    failureRedirect : '/'
+  }
+));
+
+router.post('/', function(req,res) {
+  request(
+  `${rootURL}users/${req.body.username}?access_token=${process.env.SUPERHERO_TOKEN}`,
+  function(err, response, body) {
+    res.render('index', {userData: body});
+  }
+)
+});
+
+module.exports = router;
